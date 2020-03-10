2f41d93388edc065ce4ea1ce421471660b762fcf
Shubham Kawane <kawane.s@husky.neu.edu>
2020-03-10 18:21:44 -0400

    new

--- runapp.sh
+++ runapp.sh
@@ -5,5 +5,5 @@ cd '/home/ubuntu/webapp'
 sudo mvn clean package
 cd '/home/ubuntu/webapp/target'
 sudo mkdir assets
-sudo java -jar CloudComputing-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+sudo java -jar CloudComputing-0.0.1-SNAPSHOT.jar
 
