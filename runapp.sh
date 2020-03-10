a3a9643f43af21c8e849cd7b6fb3164bc84cb3c4
Shubham Kawane <kawane.s@husky.neu.edu>
2020-03-10 16:17:17 -0400

    new

--- runapp.sh
+++ runapp.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 echo 'Starting Spring Boot'
+sudo kill -9 $(sudo lsof -t -i:8080)
 cd '/home/ubuntu/webapp'
-sudo mvn package
+sudo mvn clean package
 cd '/home/ubuntu/webapp/target'
 sudo mkdir assets
-cd '/home/ubuntu/webapp'
-sudo kill -9 $(sudo lsof -t -i:8080)
-sudo mvn spring-boot:run
+sudo java -jar CloudComputing-0.0.1-SNAPSHOT.jar
