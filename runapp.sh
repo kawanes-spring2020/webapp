bc407677f884f46e49863e5884b019e4d984cee5
Shubham Kawane <kawane.s@husky.neu.edu>
2020-03-10 16:42:19 -0400

    new

--- runapp.sh
+++ runapp.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 echo 'Starting Spring Boot'
 sudo kill -9 $(sudo lsof -t -i:8080)
-cd'/home/ubuntu/webapp/src/main/resources'
-sudo rm -rf application.properties
 cd '/home/ubuntu/webapp'
 sudo mvn clean package
 cd '/home/ubuntu/webapp/target'
