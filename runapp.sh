9f5b5d9f51577b524c55867b7fa3113130c6869d
Shubham Kawane <kawane.s@husky.neu.edu>
2020-03-12 15:56:11 -0400

    fjdfg

--- runapp.sh
+++ runapp.sh
@@ -5,5 +5,6 @@ cd '/home/ubuntu/webapp'
 sudo mvn clean package
 cd '/home/ubuntu/webapp/target'
 sudo mkdir assets
+cd '/home/ubuntu/webapp/'
 sudo nohup mvn spring-boot:run  > /dev/null 2> /dev/null < /dev/null &
 
