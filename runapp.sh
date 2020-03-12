20171942199d0b648184cd936c9753184b19d32f
Shubham Kawane <kawane.s@husky.neu.edu>
2020-03-12 16:02:41 -0400

    fjdfg

--- runapp.sh
+++ runapp.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo 'Starting Spring Boot'
-sudo kill -9 $(sudo lsof -t -i:8080)
 cd '/home/ubuntu/webapp'
 sudo mvn clean package
 cd '/home/ubuntu/webapp/target'
 sudo mkdir assets
 cd '/home/ubuntu/webapp/'
+sudo kill -9 $(sudo lsof -t -i:8080)
 sudo nohup mvn spring-boot:run  > /dev/null 2> /dev/null < /dev/null &
 
